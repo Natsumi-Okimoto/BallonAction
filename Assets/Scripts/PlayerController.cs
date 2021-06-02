f9901271e132ebef2bb2dcb55c9b077fff10bc6d
Natsumi-Okimoto <legendpocky.7230@gmail.com>
2021-06-02 18:22:45 +0900

    手順６，７

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private float scale;
     private Animator anim;
+    private string jump = "Jump";
+    public float jumpPower;
+    public bool isGrounded;
+
+    [SerializeField, Header("Linecast用 地面判定レイヤー")]
+    private LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +24,42 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+   
+    void Update()
+    {
+        // 地面接地  Physics2D.Linecastメソッドを実行して、Ground Layerとキャラのコライダーとが接地している距離かどうかを確認し、接地しているなら true、接地していないなら false を戻す
+        isGrounded = Physics2D.Linecast(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, groundLayer);
+
+        // Sceneビューに Physics2D.LinecastメソッドのLineを表示する
+        Debug.DrawLine(transform.position + transform.up * 0.4f, transform.position - transform.up * 0.9f, Color.red, 1.0f);
+
+        if (Input.GetButtonDown(jump))
+        {    // InputManager の Jump の項目に登録されているキー入力を判定する
+            Jump();
+        }
+        if (isGrounded == false && rb.velocity.y < 0.15f)
+        {
+            // 落下アニメを繰り返す
+            anim.SetTrigger("Fall");
+        }
+    }
+
+    /// <summary>
+    /// ジャンプと空中浮遊
+    /// </summary>
+    private void Jump()
+    {
+
+        // キャラの位置を上方向へ移動させる(ジャンプ・浮遊)
+        rb.AddForce(transform.up * jumpPower);
+
+        // Jump(Up + Mid) アニメーションを再生する
+        anim.SetTrigger("Jump");
+
+      
+    }
+
+    
     // Update is called once per frame
     void FixedUpdate()
     {
